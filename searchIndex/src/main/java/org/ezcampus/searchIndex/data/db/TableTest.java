1e2f04718ce0fd96a8dfdede6d43701eaf79a2cc
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-05-02 19:45:49 -0400

    basic postgres connection

---
+++ searchIndex/src/main/java/org/ezcampus/searchIndex/data/db/TableTest.java
@@ -0,0 +1,20 @@
+package org.ezcampus.searchIndex.data.db;
+
+public class TableTest {
+
+	
+    public static final String DELETION_QUERY = "DROP TABLE IF EXISTS tbl_file";
+    public static final String CREATION_QUERY = "CREATE TABLE IF NOT EXISTS tbl_file("
+            + "hash_id serial PRIMARY KEY, "
+            + "size BIGINT NOT NULL, "
+            + "mime INT NOT NULL,"
+            + "width INT NOT NULL, "
+            + "height INT NOT NULL,"
+            + "duration INT NOT NULL,"
+            + "has_audio BOOLEAN NOT NULL"
+        
+            + ");";
+    
+
+	
+}

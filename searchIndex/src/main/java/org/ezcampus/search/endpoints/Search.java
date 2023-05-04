26bb0b2f83c19f3846df63d06bbd2cec5a20f6f1
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-05-04 09:32:15 -0400

    Removed org.json and added Jackson core for json

--- searchIndex/src/main/java/org/ezcampus/search/endpoints/Search.java
+++ searchIndex/src/main/java/org/ezcampus/search/endpoints/Search.java
@@ -9,27 +9,22 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import org.json.JSONObject;
-
-// JAVAX Json Core
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Path("/search")
 public class Search {
     
-        JSONObject jsonObject = new JSONObject();
-    
+    private final ObjectMapper jsonMap = new ObjectMapper();
         
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getRequestBody(@QueryParam("limit") int limit) {
-        jsonObject.put("message", limit);
         
-        String jsonString = jsonObject.toString();
         
         return Response
                 .status(200) //Status always first! Builds 200 Response
-                .entity(jsonString) //Add JSON body
+              //  .entity(jsonString) //Add JSON body
                 .build(); 
                
     }

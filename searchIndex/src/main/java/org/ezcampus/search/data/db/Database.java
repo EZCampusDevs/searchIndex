e32cd6f6964f80d6367f2e93043dcc5bf5f7eedc
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-05-04 23:29:13 -0400

    made an index on tbl_word, and other stuff

--- searchIndex/src/main/java/org/ezcampus/search/data/db/Database.java
+++ searchIndex/src/main/java/org/ezcampus/search/data/db/Database.java
@@ -2,6 +2,8 @@ package org.ezcampus.search.data.db;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.ezcampus.search.System.GlobalSettings;
@@ -12,28 +14,41 @@ public class Database
 {
 
 	public static DatabaseConnector connector;
-	
 
 	// DEBUG ONLY
 	public static void main(String args[]) throws IOException, InterruptedException, SQLException
-    {
-        ResourceLoader.loadTinyLogConfig();
-        Logger.info("Starting...");
+	{
+		ResourceLoader.loadTinyLogConfig();
+		Logger.info("Starting...");
 
-        GlobalSettings.IS_DEBUG = true;
-        Logger.info("Running as debug: {}", GlobalSettings.IS_DEBUG);
+		GlobalSettings.IS_DEBUG = true;
+		Logger.info("Running as debug: {}", GlobalSettings.IS_DEBUG);
 
+		connector = new MySQLConnector();
+		connector.checkJDBCDriver();
+		connector.databaseName = "hibernate_db";
+		connector.username = "test";
+		connector.password = "root";
 
-        connector = new MySQLConnector();
-        connector.checkJDBCDriver();
-        connector.databaseName = "hibernate_db";
-        connector.username = "test";
-        connector.password = "root";
-        
-        Connection c = connector.getConnection("hibernate_db");
-        
-//        createNewDatabase();
-//        createTables(true);
-        
-    }
+		Connection c = connector.getConnection("hibernate_db");
+
+		final String SQL = "SELECT * FROM tbl_word LIMIT 200";
+
+		try ( PreparedStatement pstmt = c.prepareStatement(SQL))
+		{
+			pstmt.execute();
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next())
+			{
+				Logger.info(rs.getString(2));
+			}
+		}
+		catch (SQLException e)
+		{
+			Logger.error(e);
+		}
+
+	}
 }
--- searchIndex/src/main/java/org/ezcampus/search/hibernate/App.java
+++ searchIndex/src/main/java/org/ezcampus/search/hibernate/App.java
@@ -1,53 +1,65 @@
 package org.ezcampus.search.hibernate;
 
+import java.util.Arrays;
 import java.util.List;
 
-import org.ezcampus.search.hibernate.entity.Student;
+import org.ezcampus.search.System.ResourceLoader;
+import org.ezcampus.search.hibernate.entity.Word;
 import org.ezcampus.search.hibernate.util.SessionUtil;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 
 public class App
 {
 	// https://www.javaguides.net/2018/11/hibernate-hello-world-tutorial.html
-	
-	public static void main(String[] args)
+
+	public static void insertWords(List<String> words)
 	{
-		Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-		Student student1 = new Student("John", "Cena", "john@javaguides.com");
-		
-		Transaction transaction = null;
 		try (Session session = SessionUtil.getSessionFactory().openSession())
 		{
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student objects
-			session.persist(student);
-			session.persist(student1);
-			// commit transaction
-			transaction.commit();
-		}
-		catch (Exception e)
-		{
-			if (transaction != null)
+			Transaction tx = session.beginTransaction();
+			for (String word : words)
 			{
-				transaction.rollback();
+				try
+				{
+					session.persist(new Word(word));
+				}
+				catch (ConstraintViolationException ex)
+				{
+					// Log message and continue inserting the rest of the words
+					System.out.printf("Word '%s' already exists, ignoring.%n", word);
+				}
 			}
-			e.printStackTrace();
+			tx.commit();
 		}
+		catch (HibernateException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		ResourceLoader.loadTinyLogConfig();
+
+//		List<String> word1s = Arrays.asList(
+//				"new", "new2", "new3", "hello", "world", "nyah", "uwu", "test", "uwu", "nyah", "test", "alskd", "other"
+//		);
+//		insertWords(word1s);
 
 		try (Session session = SessionUtil.getSessionFactory().openSession())
 		{
-			List<Student> students = session.createQuery("from Student", Student.class).list();
-			students.forEach(s -> System.out.println(s.getFirstName()));
-		}
-		catch (Exception e)
-		{
-			if (transaction != null)
+			List<Word> words = session.createQuery("FROM Word", Word.class).list();
+			for (Word word : words)
 			{
-				transaction.rollback();
+				System.out.println(word);
 			}
-			e.printStackTrace();
+		}
+		catch (HibernateException ex)
+		{
+			ex.printStackTrace();
 		}
 	}
 }

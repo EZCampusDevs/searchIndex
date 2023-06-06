f53576340100800470f34017045293bfff294763
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-06-06 13:47:17 -0400

    Cleaned up SchoolPath request handling

--- searchIndex/src/main/java/org/ezcampus/search/api/SchoolPath.java
+++ searchIndex/src/main/java/org/ezcampus/search/api/SchoolPath.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import java.util.List;
 
-import org.ezcampus.search.hibernate.util.HibernateUtil;
-import org.hibernate.Session;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.ws.rs.Consumes;
@@ -15,12 +13,13 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+
 //Models
 import org.ezcampus.search.core.models.request.TermsQuery;
+import org.ezcampus.search.core.SchoolHandler;
 
 //Hibernate Entities:
 import org.ezcampus.search.hibernate.entity.Term;
-import org.ezcampus.search.hibernate.entity.School;
 
 
 // Nice Boilerplate with Assertion for bad Post Payloads
@@ -51,42 +50,23 @@ public class SchoolPath
         try {
 
 			TermsQuery requestData = jsonMap.readValue(jsonPayload, TermsQuery.class);
-            
-            try (Session session = HibernateUtil.getSessionFactory().openSession() )
-            {
-                        
-            String queryString = "FROM School s WHERE s.schoolUniqueValue = :sN";
-
-            School existingSchool = session.createQuery(queryString, School.class)
-                    .setParameter("sN", requestData.getSchoolName())
-                    .getSingleResult();
-
-            //Get all terms offered from that School
-            String queryString1 = "SELECT t FROM Term t WHERE t.school = :existingSchool";
-
-            List<Term> terms = session.createQuery(queryString1, Term.class)
-                    .setParameter("existingSchool", existingSchool)
-                    .getResultList();
+            List<Term> SchoolTerms = SchoolHandler.getTerms(requestData);
 
                 // Convert to JSON array string
 			try {
-				String jsonArray = jsonMap.writeValueAsString(terms);
-
+                
+				String jsonArray = jsonMap.writeValueAsString(SchoolTerms);
 				return Response.status(Response.Status.OK).entity(jsonArray).build(); // Response with 200 OK and data
-																						// (search results)
+
 			} catch (JsonProcessingException e) { //Error handling for JSON building of results
 				e.printStackTrace();
 			}
 
-
             return Response.status(Response.Status.NOT_FOUND).entity("404 not found...").build();
             
-            }
-
-
-        } catch (IOException e) {
+            } catch (IOException e) {
                 return Response.status(Response.Status.BAD_REQUEST).entity("Invalid JSON payload").build();
-        }
+            }
 
     }
 }
